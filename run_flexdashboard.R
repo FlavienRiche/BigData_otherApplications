03c7d524433a9a954f92911bdf89d24230af1fee
FlavienRiche <35363732+FlavienRiche@users.noreply.github.com>
2023-12-07 09:38:35 +0100

    flexdashboard commit

---
+++ run_flexdashboard.R
@@ -0,0 +1,11 @@
+rmarkdown::run("/Users/flav/Documents/Cours/BigData2_BDSE/dashboard/add_theme.Rmd")
+
+library(flexdashboard)
+library(readxl)
+library(DT)
+library(ggplot2)
+library(scales)
+library(plotly)
+
+
+
